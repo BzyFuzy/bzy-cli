bef93b40544ab2c8307fcdc600cde7a1128a7531
L.Naran <bzy.fuzy@gmail.com>
2022-02-04 03:02:42 +0800

    .

--- structure/routes/api/index.js
+++ structure/routes/api/index.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const apiRouter = express.Router();
-const fs = require("fs");
-
-// fs.readdir(__dirname + "/", { withFileTypes: true }, (error, files) => {
-//   const directoriesInDIrectory = files
-//     .filter((item) => item.isDirectory())
-//     .map((item) => item.name);
-//   directoriesInDIrectory.forEach((dName) =>
-//     apiRouter.use(`/${dName}`, require(`./${dName}`))
-//   );
-// });
+/* const fs = require("fs"); 
+  fs.readdir(__dirname + "/", { withFileTypes: true }, (error, files) => {
+  const directoriesInDIrectory = files
+    .filter((item) => item.isDirectory())
+    .map((item) => item.name);
+  directoriesInDIrectory.forEach((dName) =>
+    apiRouter.use(`/${dName}`, require(`./${dName}`))
+  );
+}); */
 apiRouter.get("/", function (req, res) {
   res.json({ status: "ok!" });
 });
